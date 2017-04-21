0d3cba4eb403318ee9b6c1f157a4ea0cf0ac0848
David Martinez <davidmgilo@gmail.com>
2017-04-21 17:12:27 +0200

    poster img - when the video is not loaded

--- resources/views/tinkeringVideojs.blade.php
+++ resources/views/tinkeringVideojs.blade.php
@@ -10,7 +10,7 @@
 		<div class="row">
 			<div class="col-md-9 col-md-offset-1">
 
-                <video src="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4" data-setup="{}" id="video1" class="video-js" controls autoplay>
+                <video src="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4" data-setup="{}" id="video1" class="video-js" controls poster="https://www.google.es/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwj8pIe76rXTAhXHLhoKHYKAAzQQjRwIBw&url=http%3A%2F%2Fkingofwallpapers.com%2Fimage.html&psig=AFQjCNHJfB7LExROCi3Fjg1NcOvBOxWKkA&ust=1492873808352698">
                     <p class="vjs-no-js">Sorry, el teu navegador no soporta...</p>
                 </video>
 
